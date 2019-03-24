408a61b3df332480525d2643a1dc29ae5058d344
yanomarcos <yanomarcos@gmail.com>
2019-03-24 10:30:02 -0300

    SellerService and basic FindAll

--- SalesWebMvc/Data/SeedingService.cs
+++ SalesWebMvc/Data/SeedingService.cs
@@ -9,7 +9,7 @@ namespace SalesWebMvc.Data
 {
     public class SeedingService
     {
-        private SalesWebMvcContext _context;
+        private readonly SalesWebMvcContext _context;
 
         public SeedingService(SalesWebMvcContext context)
         {
